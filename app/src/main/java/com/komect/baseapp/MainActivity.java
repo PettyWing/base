ae4a6d3c401d84af55a6c4d81dfdbaefc688b523
xieyusheng <xieyusheng@cmhi.chinamobile.com>
2019-05-20 16:21:44 +0800

    OCT REM 导入http

--- app/src/main/java/com/komect/baseapp/MainActivity.java
+++ app/src/main/java/com/komect/baseapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.komect.baseapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.komect.baseapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityMainBinding binding;
     }
 }
